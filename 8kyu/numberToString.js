18658c59fadf90acadaedd741ef31151d4f4f961
Melissa Pignalosa <m.pignalosa@hotmail.com>
2025-09-29 14:45:57 +0930

    Added two new codewars challenges

---
+++ 8kyu/numberToString.js
@@ -0,0 +1,7 @@
+//We need a function that can transform a number (integer) into a string.
+
+//My Solution
+function numberToString(num) {
+  // Return a string of the number here!
+  return num.toString()
+}
