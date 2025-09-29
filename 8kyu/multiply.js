85cc9b68949d2742a3aeb3c11ec24ea15917d857
Melissa Pignalosa <m.pignalosa@hotmail.com>
2025-09-29 14:40:10 +0930

    added new codewars challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,10 @@
+// This code does not execute properly. Try to figure out why.
+
+// multiply = function (a, b) {
+// a * b;
+// }
+
+//My Solution
+multiply = function (a, b) {
+  return a * b;
+}
